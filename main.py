b0a5c09d1c7a89ba61073f46904cfff690ab0bc2
Rubén <11651356+Rubenmp@users.noreply.github.com>
2023-10-11 01:34:14 +0200

    Clean code

--- main.py
+++ main.py
@@ -14,60 +14,66 @@ import sys
 # Avoid warning
 if not sys.warnoptions:
     import warnings
+
     warnings.simplefilter("ignore")
 
 
 # PDF Layout stuff
 def get_pdf_width_and_heigth():
-    return(A4)
+    return A4
+
 
 def get_pdf_width():
-    return(get_pdf_width_and_heigth()[0])
+    return get_pdf_width_and_heigth()[0]
+
 
 def get_pdf_heigth():
-    return(get_pdf_width_and_heigth()[1])
+    return get_pdf_width_and_heigth()[1]
 
-num_rows = 5 # Rows of cards inside the pdf
-num_columns = 4 # Number of cards per row inside the pdf
-cards_per_page = num_columns*num_rows
 
-width_margin = get_pdf_width()/30 # Empty space on the left
-height_margin = get_pdf_heigth()/27 # Empty space above
-block_size = (get_pdf_width()-2*width_margin)/num_columns + get_pdf_width()/142
-space_between_lines = block_size/8
+num_rows = 5  # Rows of cards inside the pdf
+num_columns = 4  # Number of cards per row inside the pdf
+cards_per_page = num_columns * num_rows
+
+width_margin = get_pdf_width() / 30  # Empty space on the left
+height_margin = get_pdf_heigth() / 27  # Empty space above
+block_size = (get_pdf_width() - 2 * width_margin) / num_columns + get_pdf_width() / 142
+space_between_lines = block_size / 8
 
 font_size = 12
 font_type = "Helvetica-Bold"
 
 
 def text_centered_position(index):
-    "Return text position inside an one page pdf"
-    index = (cards_per_page-index-1)
-    if (index >= 0 and index < cards_per_page):
-        index_pos = (int(index/num_columns), index%num_columns)
-        width = width_margin + block_size*index_pos[1] + block_size/2
-        height = height_margin + block_size*index_pos[0] + block_size/2 + block_size/23
-        
-        return (width, height)
+    """Return text position inside a one page pdf"""
+    index = (cards_per_page - index - 1)
+    if 0 <= index < cards_per_page:
+        index_pos = (int(index / num_columns), index % num_columns)
+        width = width_margin + block_size * index_pos[1] + block_size / 2
+        height = height_margin + block_size * index_pos[0] + block_size / 2 + block_size / 23
+
+        return width, height
     else:
-        return(None)
+        return None
+
 
 def split_text(text):
-    "Split text in several lines"
-    # TODO: Be careful if just one word is splited
-    return(textwrap.wrap(text, width=18))
+    """Split text in several lines"""
+    # TODO: Be careful if just one word is split
+    return textwrap.wrap(text, width=18)
 
-def write_text_to_pdf (text, index, canvas):
-    "Draw text inside a pdf using canvas"
+
+def write_text_to_pdf(text, index, canvas):
+    """Draw text inside a pdf using canvas"""
     splited_text = split_text(text)
 
     for i in range(len(splited_text)):
-        offset = i - len(splited_text)/2
-        if (text_centered_position(index) != None):
-            width  = text_centered_position(index)[0]
-            height = text_centered_position(index)[1] - offset*space_between_lines
+        offset = i - len(splited_text) / 2
+        if text_centered_position(index) is not None:
+            width = text_centered_position(index)[0]
+            height = text_centered_position(index)[1] - offset * space_between_lines
             canvas.drawCentredString(width, height, splited_text[i]
-            )
+                                     )
 
 
 black_cards_dir = "./Input/BlackCards/"
@@ -79,9 +85,8 @@ packet = io.BytesIO()
 can = canvas.Canvas(packet, pagesize=A4)
 can.setFont(font_type, font_size)
 
-
 pdf_page_index = 0
-card_index = 0 # Index inside one pdf page
+card_index = 0  # Index inside one pdf page
 
 # Black cards
 # black_card_text_color = colors.white
@@ -94,18 +99,18 @@ for filename in os.listdir(black_cards_dir):
         write_text_to_pdf(line, card_index, can)
         card_index += 1
 
-        if (card_index == cards_per_page):
+        if card_index == cards_per_page:
             card_index = 0
             pdf_page_index += 1
             can.showPage()
             can.setFont(font_type, font_size)
 
-if (card_index > 0):
+if card_index > 0:
     can.showPage()
     can.setFont(font_type, font_size)
     pdf_page_index += 1
     card_index = 0
-    
+
 num_black_pages = pdf_page_index
 
 # White cards
@@ -117,17 +122,16 @@ for filename in os.listdir(white_cards_dir):
         write_text_to_pdf(line, card_index, can)
         card_index += 1
 
-        if (card_index == cards_per_page):
+        if card_index == cards_per_page:
             card_index = 0
             pdf_page_index += 1
             can.showPage()
             can.setFont(font_type, font_size)
 
-        
 can.showPage()
 can.save()
 
-print ("All files have been read.")
+print("All files have been read.")
 
 # Add the watermarks and create final pdf
 new_pdf = PdfReader(packet)
@@ -136,7 +140,7 @@ output = PdfWriter()
 print("PDF pages generated (out of " + str(pdf_page_index) + "):", end='', flush=True)
 for i in range(pdf_page_index):
     existing_pdf = None
-    if (i < num_black_pages):
+    if i < num_black_pages:
         existing_pdf = PdfReader(open("./Input/CAH_BlankBlackCards.pdf", "rb"))
     else:
         existing_pdf = PdfReader(open("./Input/CAH_BlankWhiteCards.pdf", "rb"))
@@ -144,7 +148,7 @@ for i in range(pdf_page_index):
     page = existing_pdf.pages[0]
     page.merge_page(new_pdf.pages[i])
     output.add_page(page)
-    print(" " + str(i+1), end='', flush=True)
+    print(" " + str(i + 1), end='', flush=True)
 
 print(".")
 
